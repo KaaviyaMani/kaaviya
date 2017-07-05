58d8ae268f786b403f8b1e5125eb7b2b4af8de6c
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-05 18:29:04 +0530

    Create Strnt.java

---
+++ Strnt.java
@@ -0,0 +1,11 @@
+import java.io.*;
+	public class Strnt{
+	  public static void main(String args[])throws IOException{
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println("Enter the string");
+	     String s=br.readLine();
+	     int i= Integer.parseInt(s);
+	      System.out.println(sum);
+	      
+	      }
+	      }
