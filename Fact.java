d4556d2ddc1431e8a54eeeb21e707a6fdbcec802
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-05 16:42:59 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+class Fact{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=n;    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial is"+fact);    
+ }  
+}  
