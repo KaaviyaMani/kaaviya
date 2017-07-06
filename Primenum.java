db1204f055c7ba30ec471ce5be1deb010a2e0d95
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-06 10:31:49 +0530

    Create Primenum.java

---
+++ Primenum.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+public class Primenum{
+  
+  public static void main(String args[])throws IOException{
+    Scanner sc=new Scanner(System.in);
+    int a,b,i=0,j=0,cou=0;
+    char []c=new char[200];
+     System.out.println("Enter the Starting number");
+    a=sc.nextInt();
+    System.out.println("Enter the Ending number");
+    b=sc.nextInt();
+       for(i=a;i>=a&&i<=b;i++)
+       {
+         if(i<=1)
+         {
+           
+           i=2;
+           }
+
+         int m=i/2;
+         for(j=2;j<m;j++)
+         {
+           
+           if(i%j==0)
+           {
+             
+             c[i]=1;
+             break;
+             }
+             }
+             if(c[i]==0)
+             {
+               
+               
+               cou++;
+               }
+               }
+               
+
+               System.out.println(cou);
+               }
+               }
+
+
+
+
+    
+    
+    
+    
+  
