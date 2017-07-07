a0abe2d1170ec35190c8bc4aa96b2b4764cd389a
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-07 10:12:42 +0530

    Create Evennum.java

---
+++ Evennum.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Evennum{
+  
+   public static void main(String args[])throws IOException{
+      Scanner sc=new Scanner(System.in);
+      int a,b;
+      System.out.println("Enter the Starting number");
+      a=sc.nextInt();
+      System.out.println("Enter the Ending number");
+      b=sc.nextInt();
+      for(int i=a;i<=b;i++)
+      {
+        
+        if(i%2==0)
+        {
+          System.out.println(i);
+          }
+          }
+          }
+          }
+
+
+ 
