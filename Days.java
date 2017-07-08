9e1e5bbf60b9b89de1ec7c84504adf6bc0ebcd06
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-08 11:54:24 +0530

    Create Days.java

---
+++ Days.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Days{
+  
+  public static void main(String args[])throws IOException{
+    
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     String a;
+     System.out.println("Enter the day");
+     a=br.readLine();
+     a=a.toLowerCase();
+     if( a.equals("sunday"))
+     {
+       
+       System.out.println("false");
+       }
+       else
+       {
+         
+         System.out.println("true");
+         }
+          }
+           }
+
